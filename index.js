d8752a21d2d1dc9d2c3c7f5bdcc01c08afdd3834
Alexandra Kononova <akononova93@gmail.com>
2024-10-21 12:01:22 -0400

    Initialize project

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const BACKEND_URL = process.env.BACKEND_URL || `http://localhost:${PORT}`;
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
+
+app.listen(PORT, () => {
+    console.log(`Listening at ${BACKEND_URL}`);
+});
